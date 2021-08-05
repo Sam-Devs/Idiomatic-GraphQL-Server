870b55f96da29cdcb30ec26a673fc8da9cc24ff6
Sam Ayorinde <ayorinde2@gmail.com>
2021-08-05 03:48:45 +0100

    modified

---
+++ resolvers/Mutation.js
@@ -0,0 +1,35 @@
+const signup = async (parent, args, context) => {
+  const password = await bcrypt.hash(args.password, 10);
+
+  const user = await context.prisma.user.create({
+    data: {
+      ...args,
+      password,
+    },
+  });
+
+  const token = jwt.sign({ userId: user.id}, APP_SECRET);
+
+  return { user, token };
+};
+
+const login = async (parent, args, context) => {
+  const user = await context.prisma.user.findUnique({
+    where: { email: args.email },
+  });
+
+  if(!user) {
+      throw new Error("User not found");
+  }
+
+  const validate = await bcrypt.compare(args.email, user.password);
+  if(!validate) {
+      throw new Error("Invalid Password")
+  }
+
+  const token = jwt.sign({ userId: user.id}, APP_SECRET)
+
+  return {user, token}
+};
+
+module.exports = { signup, login}
